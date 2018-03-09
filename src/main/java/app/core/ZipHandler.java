25e0648d27857f4038edeed602aaa2128a986f99
Emilio Castellano <ecastel6@gmail.com>
2018-03-09 18:57:11 +0100

    ZipHandler

--- src/main/java/app/core/ZipHandler.java
+++ src/main/java/app/core/ZipHandler.java
@@ -6,6 +6,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZipHandler {
@@ -13,51 +14,62 @@ public class ZipHandler {
     List<String> fileList;
     //    private static final String OUTPUT_ZIP_FILE = "C:\\tmp\\ziptest.zip";
 //    private static final String SOURCE_FOLDER = "C:\\tmp\\ziptest";
-    String sourceFolder;
-    String OutputZip;
+    String Folder;
+    String ZipFile;
 
-    public ZipHandler(String sourceFolder, String outputZip) {
-        this.sourceFolder = sourceFolder;
-        this.OutputZip = outputZip;
+    public ZipHandler() {
         fileList = new ArrayList<String>();
-        generateFileList(new File(sourceFolder));
+    }
+
+    public String getFolder() {
+        return Folder;
+    }
+
+    public void setFolder(String folder) {
+        Folder = folder;
+    }
+
+    public String getZipFile() {
+        return ZipFile;
+    }
+
+    public void setZipFile(String zipFile) {
+        ZipFile = zipFile;
     }
 
     /**
-     * Zipit
+     * compress
      *
+     * @param sourceFolder
+     * @param outputZip
      * @param level compression level
      */
-    public void zipIt(int level) {
+    public void zip(String sourceFolder, String outputZip, int level) {
         byte[] buffer = new byte[1024];
-
+        this.setFolder(sourceFolder);
+        this.setZipFile(outputZip);
+        generateFileList(new File(sourceFolder));
         try {
-
-            FileOutputStream fos = new FileOutputStream(this.OutputZip);
-            ZipOutputStream zos = new ZipOutputStream(fos);
-            zos.setLevel(level);
+            FileOutputStream fileOutputStream = new FileOutputStream(outputZip);
+            ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
+            zipOutputStream.setLevel(level);
             for (String file : this.fileList) {
-                File f = new File(sourceFolder + File.separator + file);
-                //System.out.printf("File Added : %s. LastModified: %s\n",f.toString()+String.valueOf(f.lastModified()));
-                ZipEntry ze = new ZipEntry(file);
-                ze.setTime(f.lastModified());
-                zos.putNextEntry(ze);
+                File absoluteFilename = new File(sourceFolder + File.separator + file);
+                ZipEntry zipEntry = new ZipEntry(file);
+                zipEntry.setTime(absoluteFilename.lastModified());
+                zipOutputStream.putNextEntry(zipEntry);
 
-                FileInputStream in =
+                FileInputStream fileInputStream =
                         new FileInputStream(sourceFolder + File.separator + file);
 
-                int len;
-                while ((len = in.read(buffer)) > 0) {
-                    zos.write(buffer, 0, len);
+                int length;
+                while ((length = fileInputStream.read(buffer)) > 0) {
+                    zipOutputStream.write(buffer, 0, length);
                 }
-
-                in.close();
+                fileInputStream.close();
             }
-
-            zos.closeEntry();
-            //remember close it
-            zos.close();
-
+            zipOutputStream.closeEntry();
+            zipOutputStream.close();
             System.out.println("Done");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -94,11 +106,71 @@ public class ZipHandler {
      * @return Formatted file path
      */
     private String generateZipEntry(String file) {
-        return file.substring(sourceFolder.length() + 1, file.length());
+        return file.substring(this.getFolder().length() + 1, file.length());
     }
 
+    /**
+     * unzip zipfile to outputFolder
+     *
+     * @param zipFile
+     * @param outputFolder
+     * @return void
+     */
+    public void unzip(String zipFile, String outputFolder) {
+
+        byte[] buffer = new byte[1024];
+        try {
+            File folder = new File(outputFolder);
+            if (!folder.exists()) {
+                folder.mkdir();
+            }
+
+            //get the zip file content
+            ZipInputStream zipInputStream =
+                    new ZipInputStream(new FileInputStream(zipFile));
+            //get the zipped file list entry
+            ZipEntry zipEntry = zipInputStream.getNextEntry();
+
+            while (zipEntry != null) {
+
+                String fileName = zipEntry.getName();
+                File newFile = new File(outputFolder + File.separator + fileName);
+                System.out.println("file unzip : " + newFile.getAbsoluteFile());
+
+                //create all non existing folders
+                new File(newFile.getParent()).mkdirs();
+                FileOutputStream fileOutputStream = new FileOutputStream(newFile);
+
+                int length;
+                while ((length = zipInputStream.read(buffer)) > 0) {
+                    fileOutputStream.write(buffer, 0, length);
+                }
+                fileOutputStream.close();
+
+                if (!newFile.setLastModified(zipEntry.getTime()))
+                    System.out.println("ERROR: Unable to set file timestamp");
+                zipEntry = zipInputStream.getNextEntry();
+            }
+
+            zipInputStream.closeEntry();
+            zipInputStream.close();
+            System.out.println("Done");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        ZipHandler appZip = new ZipHandler("c:\\tmp\\ziptest", "c:\\tmp\\ziptest.zip");
-        appZip.zipIt(CompresionLevel.UNCOMPRESSED.getLevel());
+        ZipHandler sampleZip = new ZipHandler();
+        sampleZip.zip(
+                "c:\\tmp\\ziptest",
+                "c:\\tmp\\ziptest.zip",
+                CompresionLevel.UNCOMPRESSED.getLevel());
+
+        sampleZip.unzip("c:\\tmp\\ziptest.zip", "c:\\tmp\\ziptest2");
+
     }
 }
